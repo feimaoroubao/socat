1974eddb3ebed8d99ea30c1606ab47103df8f2c2
feimaoroubao <122804672+feimaoroubao@users.noreply.github.com>
2024-11-30 21:30:15 +0800

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -1,49 +1,41 @@
-#! /bin/bash
+#!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# ====================================================
-# System Request:CentOS 6+ 、Debian 7+、Ubuntu 14+
-# Author: Rat's
-# Dscription: Socat一键脚本
-# Version: 2.1
-# Blog: http://www.vipkj.net
-# Github:https://github.com/lzw981731/socat
-# ====================================================
-
+# Color codes
 Green="\033[32m"
 Font="\033[0m"
 Blue="\033[33m"
+Red="\033[31m"
 CONFIG_FILE="/root/socat_config.txt"
 
+# Function to error out if not run as root
 rootness(){
     if [[ $EUID -ne 0 ]]; then
-       echo "Error: This script must be run as root!" 1>&2
-       exit 1
+        echo -e "${Red}Error: This script must be run as root!${Font}"
+        exit 1
     fi
 }
 
+# Function to check and identify OS
 checkos(){
-    if [[ -f /etc/redhat-release ]]; then
+    if grep -q 'CentOS,release,([0-6])' /etc/redhat-release; then
         OS=CentOS
     elif cat /etc/issue | grep -q -E -i "debian"; then
         OS=Debian
     elif cat /etc/issue | grep -q -E -i "ubuntu"; then
         OS=Ubuntu
-    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-        OS=CentOS
     elif cat /proc/version | grep -q -E -i "debian"; then
         OS=Debian
     elif cat /proc/version | grep -q -E -i "ubuntu"; then
         OS=Ubuntu
-    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-        OS=CentOS
     else
-        echo "Not supported OS, Please reinstall OS and try again."
+        echo -e "${Red}Not supported OS, Please reinstall OS and try again.${Font}"
         exit 1
     fi
 }
 
+# Function to disable SELinux
 disable_selinux(){
     if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
         sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
@@ -51,6 +43,7 @@ disable_selinux(){
     fi
 }
 
+# Function to disable iptables and Firewalld
 disable_iptables(){
     systemctl stop firewalld.service >/dev/null 2>&1
     systemctl disable firewalld.service >/dev/null 2>&1
@@ -58,10 +51,12 @@ disable_iptables(){
     chkconfig iptables off >/dev/null 2>&1
 }
 
+# Function to get public IP
 get_ip(){
     ip=`curl -s http://whatismyip.akamai.com`
 }
 
+# Function to configure Socat
 config_socat(){
     echo -e "${Green}请输入Socat配置信息！${Font}"
     read -p "请输入本地端口:" port1
@@ -119,79 +114,82 @@ config_socat(){
     echo "------------------------" >> $CONFIG_FILE
 }
 
+# Function to start Socat and configure the forwarding
 start_socat(){
     # 从配置文件中读取最新配置
-    if [[ -f $CONFIG_FILE ]]; then
-        source $CONFIG_FILE
-    else
-        echo "配置文件不存在，无法启动Socat。"
+    if [[ ! -f $CONFIG_FILE ]]; then
+        echo -e "${Red}配置文件不存在，无法启动Socat, 请先进行配置。${Font}"
         exit 1
     fi
+    source $CONFIG_FILE
 
     echo -e "${Green}正在配置Socat...${Font}"
-    
-    # 启动对应的 Socat 进程
+
     if [[ "$protocol" == "TCP" ]]; then
-        if [[ "$ip_version" == "IPv4" ]]; then
-            nohup socat TCP4-LISTEN:${port1},reuseaddr,fork TCP4:${socatip}:${port2} >> /root/socat.log 2>&1 &
-        else
-            nohup socat TCP6-LISTEN:${port1},reuseaddr,fork TCP6:${socatip}:${port2} >> /root/socat.log 2>&1 &
-        fi
+        OPT="TCP"
     elif [[ "$protocol" == "UDP" ]]; then
-        if [[ "$ip_version" == "IPv4" ]]; then
-            nohup socat UDP4-LISTEN:${port1},reuseaddr,fork UDP4:${socatip}:${port2} >> /root/socat.log 2>&1 &
-        else
-            nohup socat UDP6-LISTEN:${port1},reuseaddr,fork UDP6:${socatip}:${port2} >> /root/socat.log 2>&1 &
-        fi
-    elif [[ "$protocol" == "TCP+UDP" ]]; then
-        if [[ "$ip_version" == "IPv4" ]]; then
-            nohup socat TCP4-LISTEN:${port1},reuseaddr,fork TCP4:${socatip}:${port2} >> /root/socat.log 2>&1 &
-            nohup socat UDP4-LISTEN:${port1},reuseaddr,fork UDP4:${socatip}:${port2} >> /root/socat.log 2>&1 &
-        else
-            nohup socat TCP6-LISTEN:${port1},reuseaddr,fork TCP6:${socatip}:${port2} >> /root/socat.log 2>&1 &
-            nohup socat UDP6-LISTEN:${port1},reuseaddr,fork UDP6:${socatip}:${port2} >> /root/socat.log 2>&1 &
-        fi
+        OPT="UDP"
+    else
+        OPT="TCP,UDP"
     fi
 
-    get_ip
-    sleep 3
-    echo
-    echo -e "${Green}Socat安装并配置成功!${Font}"
-    echo -e "${Blue}你的本地端口为:${port1}${Font}"
-    echo -e "${Blue}你的远程端口为:${port2}${Font}"
-    echo -e "${Blue}你的本地服务器IP为:${ip}${Font}"
+    IP_VER=$( [[ "$ip_version" == "IPv4" ]] && echo "-4" || echo "-6" )
+    
+    echo -e "${Blue}Starting Socat Forwarding Channel${Font}\n"
+    echo -e "Protocol: ${protocol}"
+    echo -e "IP Version: ${ip_version}"
+    echo -e "Port Forwarding: ${port1} -> ${socatip}:${port2}" 1>&2
 
-    # 添加到 /etc/rc.local
-    if [[ -f /etc/rc.local ]]; then
-        if ! grep -q "socat TCP4-LISTEN:${port1},reuseaddr,fork TCP4:${socatip}:${port2}" /etc/rc.local; then
-            echo "nohup socat TCP4-LISTEN:${port1},reuseaddr,fork TCP4:${socatip}:${port2} >> /root/socat.log 2>&1 &" >> /etc/rc.local
-        fi
-        if ! grep -q "socat UDP4-LISTEN:${port1},reuseaddr,fork UDP4:${socatip}:${port2}" /etc/rc.local; then
-            echo "nohup socat UDP4-LISTEN:${port1},reuseaddr,fork UDP4:${socatip}:${port2} >> /root/socat.log 2>&1 &" >> /etc/rc.local
-        fi
-    fi
+    # 检查并停止在同一端口启动的社威特程
+    pkill -f "socat $OPT$IP_VER-LISTEN:$port1"
 
-    # 创建 Systemd 服务
-    cat <<EOF > /etc/systemd/system/socat.service
+    # 启动对应的 Socat 进程
+    nohup socat $OPT$IP_VER-LISTEN:$port1,reuseaddr,fork $OPT$IP_VER:$socatip:$port2 >> /root/socat.log 2>&1 &
+
+    # 等待几秒钟，确保进程细动了
+    sleep 5
+
+    # 检查进程是否启动哈成
+    LISTENING=$(netstat -tuln | grep ${port1})
+    if [[ $? -ne 0 ]]; then
+        echo -e "${Red}启动失败，请检查下面的日志：${Font}/root/socat.log"
+    else
+        get_ip
+        echo -e "\n${Green}"
+        echo "Socat Forwarding Configured Successfully!"
+        echo -e "Your local port is: ${port1}"
+        echo -e "Your remote port is: ${port2}"
+        echo -e "Your local server IP is: ${ip}"
+        echo -e "${Font}"
+
+        # 添加到启动脚本
+        if ! grep -q "socat $OPT$IP_VER-LISTEN:$port1" /etc/rc.local; then
+            sed "nohup socat $OPT$IP_VER-LISTEN:${port1},reuseaddr,fork $OPT$IP_VER:${socatip}:${port2} >> /root/socat.log 2>&1 &" >> /etc/rc.local
+        fi
+
+        # 创建 Systemd 服务
+        cat << EOF > /etc/systemd/system/socat.service
 [Unit]
 Description=Socat Service
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/socat TCP4-LISTEN:${port1},reuseaddr,fork TCP4:${socatip}:${port2}
+ExecStart=/usr/bin/socat $OPT$IP_VER-LISTEN:$port1,reuseaddr,fork $OPT$IP_VER:$socatip:$port2
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-    # 启用和启动服务
-    systemctl daemon-reload
-    systemctl enable socat.service
-    systemctl start socat.service
+        # 启用和启动服务
+        systemctl daemon-reload
+        systemctl enable socat.service
+        systemctl start socat.service
+    fi
 }
 
+# Function to install Socat
 install_socat(){
     echo -e "${Green}即将安装Socat...${Font}"
     if [ "${OS}" == 'CentOS' ]; then
@@ -201,172 +199,20 @@ install_socat(){
         apt-get install -y socat
     fi
     if [ -s /usr/bin/socat ]; then
-        echo -e "${Green}Socat安装完成！${Font}"
+        echo -e "${Green}Socat已经安装完成!${Font}"
+    else
+        echo -e "${Red}安装Socat失败，请检查包管理器或手动安装。${Font}"
+        exit 1
     fi
 }
 
+# Function to check Socat status and all the rest
 status_socat(){
     if [ -s /usr/bin/socat ]; then
         echo -e "${Green}检测到Socat已存在，并跳过安装步骤！${Font}"
-        main_x
     else
         main_y
     fi
-}
-
-query_socat(){
-    echo -e "${Green}当前Socat转发信息:${Font}"
-
-    # 从配置文件中读取
-    if [[ -f $CONFIG_FILE ]]; then
-        while IFS= read -r line; do
-            if [[ $line == *"port"* || $line == *"protocol"* || $line == *"ip_version"* ]]; then
-                echo "$line"
-            elif [[ $line == *"------------------------"* ]]; then
-                echo "------------------------"
-            fi
-        done < $CONFIG_FILE
-    else
-        echo "没有找到配置文件，无法显示转发信息。请先进行配置。"
-        return
-    fi
-
-    cat /root/socat.log | tail -n 10
-}
-
-modify_socat(){
-    echo -e "${Green}请输入要修改的Socat配置信息！${Font}"
-    read -p "请输入要修改的本地端口:" old_port1
-    read -p "请输入新的本地端口:" new_port1
-    read -p "请输入新的远程端口:" new_port2
-    read -p "请输入新的远程IP:" new_socatip
-
-    # 停止当前的Socat进程
-    pkill socat
-
-    # 读取现有配置并替换
-    if [[ -f $CONFIG_FILE ]]; then
-        temp_file=$(mktemp)
-        while IFS= read -r line; do
-            if [[ $line == port1* && $line == *"$old_port1"* ]]; then
-                echo "port1=${new_port1}" >> "$temp_file"
-                echo "port2=${new_port2}" >> "$temp_file"
-                echo "socatip=${new_socatip}" >> "$temp_file"
-                # 读取并替换协议
-                read -r # Skip the next line
-                protocol_choice=0
-                while [[ $protocol_choice -eq 0 ]]; do
-                    echo -e "${Green}请选择协议类型:${Font}"
-                    echo -e "1. TCP"
-                    echo -e "2. UDP"
-                    echo -e "3. TCP和UDP"
-                    read -p "请输入选择（1/2/3）:" protocol_choice
-                    case $protocol_choice in
-                        1)
-                            echo "protocol=TCP" >> "$temp_file"
-                            ;;
-                        2)
-                            echo "protocol=UDP" >> "$temp_file"
-                            ;;
-                        3)
-                            echo "protocol=TCP+UDP" >> "$temp_file"
-                            ;;
-                        *)
-                            echo "无效选择，默认使用TCP。"
-                            echo "protocol=TCP" >> "$temp_file"
-                            ;;
-                    esac
-                done
-                # 读取并替换IP版本
-                echo -e "${Green}请选择远程IP类型:${Font}"
-                echo -e "1. IPv4"
-                echo -e "2. IPv6"
-                read -p "请输入选择（1/2）:" ip_version_choice
-
-                case $ip_version_choice in
-                    1)
-                        echo "ip_version=IPv4" >> "$temp_file"
-                        ;;
-                    2)
-                        echo "ip_version=IPv6" >> "$temp_file"
-                        ;;
-                    *)
-                        echo "无效选择，默认使用IPv4。"
-                        echo "ip_version=IPv4" >> "$temp_file"
-                        ;;
-                esac
-
-                echo "------------------------" >> "$temp_file"
-                # 跳过后续的旧配置
-                read -r # Skip the next lines for the old configuration
-                read -r
-                read -r
-            else
-                echo "$line" >> "$temp_file"
-            fi
-        done < "$CONFIG_FILE"
-        mv "$temp_file" "$CONFIG_FILE"
-    else
-        echo "没有找到配置文件，无法修改转发信息。"
-        return
-    fi
-
-    # 启动新的 Socat 进程
-    start_socat
-}
-
-delete_socat(){
-    echo -e "${Green}请输入要删除的Socat转发信息！${Font}"
-    read -p "请输入要删除的本地端口:" del_port
-
-    # 停止当前的Socat进程
-    pkill socat
-
-    # 读取现有配置并删除
-    if [[ -f $CONFIG_FILE ]]; then
-        temp_file=$(mktemp)
-        while IFS= read -r line; do
-            if [[ $line == port1* && $line == *"$del_port"* ]]; then
-                # 跳过后续行以删除整个配置
-                read -r
-                read -r
-                read -r
-                read -r # Skip the separator line
-                echo "转发信息已删除。"
-            else
-                echo "$line" >> "$temp_file"
-            fi
-        done < "$CONFIG_FILE"
-        mv "$temp_file" "$CONFIG_FILE"
-    else
-        echo "没有找到配置文件，无法删除转发信息。"
-        return
-    fi
-
-    echo "请确认是否重新启动剩余的转发？(y/n)"
-    read -r restart_choice
-    if [[ $restart_choice == "y" ]]; then
-        start_socat
-    fi
-}
-
-main_x(){
-    checkos
-    rootness
-    disable_selinux
-    disable_iptables
-    config_socat
-    start_socat
-}
-
-main_y(){
-    checkos
-    rootness
-    disable_selinux
-    disable_iptables
-    install_socat
-    config_socat
-    start_socat
 }
 
 # 新增选项
@@ -375,7 +221,7 @@ echo -e "1. 查询Socat转发信息"
 echo -e "2. 修改Socat转发信息"
 echo -e "3. 安装并配置Socat"
 echo -e "4. 删除Socat转发信息"
-read -p "请输入选择（1/2/3/4）:" choice
+read -p "选择（1|2|3|4）:" choice
 
 case $choice in
     1)
@@ -395,3 +241,23 @@ case $choice in
         exit 1
         ;;
 esac
+
+# 主要调用函数
+main_x(){
+    checkos
+    rootness
+    disable_selinux
+    disable_iptables
+    config_socat
+    start_socat
+}
+
+main_y(){
+    checkos
+    rootness
+    disable_selinux
+    disable_iptables
+    install_socat
+    config_socat
+    start_socat
+}
